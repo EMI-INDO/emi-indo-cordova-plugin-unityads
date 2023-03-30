e732d70c8a99944d5a9513069a73333bdaac0595
EMI-INDO <78555833+EMI-INDO@users.noreply.github.com>
2023-03-30 21:42:36 +0700

    Update emiUnityAdsPlugin.js

--- www/emiUnityAdsPlugin.js
+++ www/emiUnityAdsPlugin.js
@@ -1,21 +1,21 @@
 var exec = require('cordova/exec');
 
-exports.unityInitialize = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'unityInitialize', [arg0]);
+exports.unitySdkInitialize = function (arg0, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'unitySdkInitialize', [arg0]);
 };
 
-exports.showBanner = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'showBanner', [arg0]);
+exports.showBannerAd = function (arg0, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'showBannerAd', [arg0]);
 };
 
-exports.hideBanner = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'hideBanner', [arg0]);
+exports.hideBannerAd = function (arg0, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'hideBannerAd', [arg0]);
 };
 
-exports.showInterstitial = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'showInterstitial', [arg0]);
+exports.showInterstitialAd = function (arg0, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'showInterstitialAd', [arg0]);
 };
 
-exports.showVideo = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'showVideo', [arg0]);
+exports.showVideoAd = function (arg0, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'showVideoAd', [arg0]);
 };
