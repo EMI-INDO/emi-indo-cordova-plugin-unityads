fad11ff88f82187235dd68fc42c55d24ca79d7f7
EMI-INDO <78555833+EMI-INDO@users.noreply.github.com>
2023-06-26 17:43:48 +0700

    New Api, New Example
    New Api, New Example

--- www/emiUnityAdsPlugin.js
+++ www/emiUnityAdsPlugin.js
@@ -4,18 +4,38 @@ exports.unitySdkInitialize = function (arg0, success, error) {
     exec(success, error, 'emiUnityAdsPlugin', 'unitySdkInitialize', [arg0]);
 };
 
-exports.showBannerAd = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'showBannerAd', [arg0]);
+exports.debugLoadAdError = function (arg0, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'debugLoadAdError', [arg0]);
 };
 
-exports.hideBannerAd = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'hideBannerAd', [arg0]);
+exports.setPrivacyAndConsent = function (arg0, arg1, arg2, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'setPrivacyAndConsent', [arg0, arg1, arg2]);
 };
 
-exports.showInterstitialAd = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'showInterstitialAd', [arg0]);
+exports.loadBannerAd = function (arg0, arg1, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'loadBannerAd', [arg0, arg1]);
 };
 
-exports.showVideoAd = function (arg0, success, error) {
-    exec(success, error, 'emiUnityAdsPlugin', 'showVideoAd', [arg0]);
+exports.showBannerAd = function (success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'showBannerAd', []);
+};
+
+exports.hideBannerAd = function (success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'hideBannerAd', []);
+};
+
+exports.loadInterstitialAd = function (arg0, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'loadInterstitialAd', [arg0]);
+};
+
+exports.showInterstitialAd = function (success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'showInterstitialAd', []);
+};
+
+exports.loadVideoAd = function (arg0, success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'loadVideoAd', [arg0]);
+};
+
+exports.showVideoAd = function (success, error) {
+    exec(success, error, 'emiUnityAdsPlugin', 'showVideoAd', []);
 };
